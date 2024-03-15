4032c03f343aa6a84b7c31fd0556617ea99ffa91
Lulu828-cyber <146968254+Lulu828-cyber@users.noreply.github.com>
2024-03-15 20:32:51 +0000

    necesitaGuardarse

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [inputValue, setImputValue]= useState ("")
+	const [todos, setTodos] = useState([]);
+
+
+
 	return (
-		<div className="Todolist">
-			<h2>ToDoList</h2>
-		</div>
-	)
-};
+		<div className="container text-center">
+			<h1>To do List</h1>
+			<ul>
+				
+				<li>
+					<input 
+			type="text"
+			onChange= {(e) => setImputValue (e.target.value)}
+			value= {inputValue}
+			onkeyPress= {(e) => {
+				if (e.onkeyPress ==="Enter"){
+					setTodos (todos.concat (inputValue));
+					setImputValue ("");
+				}
+			}}
+			placeholder="your morning routine..."></input>
+			 
+    </li>
+       {todos.map((item, index) => (
+		<li>
+			{item}{""}
+			<i 
+			class="fa-thin fa-trash"
+			onClick={() =>
+			setTodos (
+				todos.filter(
+					(item, currentIndex) => index != currentIndex
+				)
+			)
+			}></i>
+
+		</li>
+	   ))}
+
+	</ul>
+
+    <div>{todos.length} tasks</div>
+
+</div>)}
 
 export default Home;
